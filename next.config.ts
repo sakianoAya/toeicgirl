eca64a7fe53f0f8067c714e92ce429067811af3a
sakianoaya <qwrr871210@gmail.com>
2025-02-06 10:43:18 +0900

    delete nextconfigts-setting

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // 忽略 ESLint 錯誤
   },
-  // 確保 Prisma 生成客戶端
-  postinstall: "prisma generate", // 安裝依賴後自動生成 Prisma 客戶端
 };
 
 export default nextConfig;
